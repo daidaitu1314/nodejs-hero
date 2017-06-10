b216953bc865216eb566b4c265c4de78039ddc79
daidaitu1314 <770104768@qq.com>
2017-06-10 17:18:31 +0800

    完成列表的渲染与英雄的添加

---
+++ render.js
@@ -0,0 +1,17 @@
+var template = require('art-template');
+var path = require('path');
+
+module.exports = function (res) {
+  // 为 res 对象绑定自定义的 render 函数，调用时候需要传递文件名和要渲染的数据对象
+  res.render = function (filename, data) {
+    // 使用art-template来渲染页面HTMl
+    var html = template(path.join(__dirname, './views/' + filename + '.html'), data || {});
+    // 将渲染出来的HTMl发送到页面上显示
+    res.end(html);
+  }
+
+  // 将数据对象转成JSON字符串返回
+  res.json = function (dataObj) {
+    res.end(JSON.stringify(dataObj));
+  }
+}
